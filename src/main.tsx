6f2439778efc4708c7ecedbf96d8fc759da09097
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-07-10 20:59:31 +0800

    update the create root, to check that the render is not null

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root")!; // non-null assertion
+
+// the ! tells typescript "trust me, this won't be null"
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
